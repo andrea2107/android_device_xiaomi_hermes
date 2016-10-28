e126ca2430b3b93f9e9aa3488472d2f4d949c60a
Lok Jian Ming <jianminglok@hotmail.com>
2016-10-28 10:09:44 +0800

    Update mokee.mk

--- mokee.mk
+++ mokee.mk
@@ -1,11 +1,15 @@
 $(call inherit-product, device/xiaomi/hermes/device_hermes.mk)
 
-# Inherit some common MK stuff.
-$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+# Inherit 64-bit configs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/xiaomi/hermes/overlay
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hermes
-PRODUCT_NAME := mk_hermes
+PRODUCT_NAME := aosp_hermes
 PRODUCT_BRAND := XiaoMi
 PRODUCT_MODEL := Redmi Note 2
 PRODUCT_MANUFACTURER := XiaoMi

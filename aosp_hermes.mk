8d32c8a607e83e03fa674c25b2917f0bae53ce21
Lok Jian Ming <jianminglok@hotmail.com>
2016-10-28 10:11:17 +0800

    Update aosp_hermes.mk

--- aosp_hermes.mk
+++ aosp_hermes.mk
@@ -1,10 +1,5 @@
 $(call inherit-product, device/xiaomi/hermes/device_hermes.mk)
 
-# Inherit 64-bit configs
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 DEVICE_PACKAGE_OVERLAYS += device/xiaomi/hermes/overlay
 
 ## Device identifier. This must come after all inclusions

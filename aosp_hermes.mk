97a10236928a47ec30c6b14f9613c6b9a4911727
Lok Jian Ming <jianminglok@hotmail.com>
2016-10-30 17:02:04 +0800

    Update aosp_hermes.mk

--- aosp_hermes.mk
+++ aosp_hermes.mk
@@ -1,15 +1,1 @@
-$(call inherit-product, device/xiaomi/hermes/omni_hermes.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/xiaomi/hermes/overlay
-
-# Get the prebuilt list of APNs
-$(call inherit-product, vendor/omni/config/gsm.mk)
-
-$(call inherit-product, vendor/omni/config/common.mk)
-
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := hermes
-PRODUCT_NAME := aosp_hermes
-PRODUCT_BRAND := XiaoMi
-PRODUCT_MODEL := Redmi Note 2
-PRODUCT_MANUFACTURER := XiaoMi

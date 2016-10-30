3f30b1b47ab8e0d67bb2a4cd70df177c0a531976
Lok Jian Ming <jianminglok@hotmail.com>
2016-10-30 18:04:42 +0800

    Update omni_hermes.mk

--- omni_hermes.mk
+++ omni_hermes.mk
@@ -6,6 +6,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # Get the prebuilt list of APNs
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
+# must be before including omni part
+TARGET_BOOTANIMATION_SIZE := 1080x608
+
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from hardware-specific part of the product configuration
@@ -13,11 +16,6 @@ $(call inherit-product, device/xiaomi/hermes/device.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := xiaomi hermes
-
-# Boot animation
-TARGET_SCREEN_HEIGHT      := 1920
-TARGET_SCREEN_WIDTH       := 1080
-TARGET_BOOTANIMATION_NAME := 1080
 
 # LANGUAGE
 PRODUCT_DEFAULT_LANGUAGE := en

7a3b1ac4be94e57ba927db8231e78b35999eb255
Lok Jian Ming <jianminglok@hotmail.com>
2016-10-30 16:04:53 +0800

    Update aosp_hermes.mk

--- aosp_hermes.mk
+++ aosp_hermes.mk
@@ -2,6 +2,8 @@ $(call inherit-product, device/xiaomi/hermes/omni_hermes.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/xiaomi/hermes/overlay
 
+$(call inherit-product, vendor/omni/config/common.mk)
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hermes
 PRODUCT_NAME := aosp_hermes

71a57e96b089e7a9ee9cc407705cdb4c867f9ebd
Lok Jian Ming <jianminglok@hotmail.com>
2016-10-30 17:01:52 +0800

    Update omni_hermes.mk

--- omni_hermes.mk
+++ omni_hermes.mk
@@ -1,14 +1,15 @@
+DEVICE_PACKAGE_OVERLAYS += device/xiaomi/hermes/overlay
+
+# Get the prebuilt list of APNs
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+$(call inherit-product, vendor/omni/config/common.mk)
+
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/xiaomi/hermes/device.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := xiaomi hermes
-
-PRODUCT_DEVICE := hermes
-PRODUCT_NAME := omni_hermes
-PRODUCT_BRAND := xiaomi
-PRODUCT_MODEL := Redmi Note 2
-PRODUCT_MANUFACTURER := xiaomi
 
 # Boot animation
 TARGET_SCREEN_HEIGHT      := 1920
@@ -18,3 +19,10 @@ TARGET_BOOTANIMATION_NAME := 1080
 # LANGUAGE
 PRODUCT_DEFAULT_LANGUAGE := en
 PRODUCT_DEFAULT_REGION   := US
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := hermes
+PRODUCT_NAME := aosp_hermes
+PRODUCT_BRAND := XiaoMi
+PRODUCT_MODEL := Redmi Note 2
+PRODUCT_MANUFACTURER := XiaoMi
